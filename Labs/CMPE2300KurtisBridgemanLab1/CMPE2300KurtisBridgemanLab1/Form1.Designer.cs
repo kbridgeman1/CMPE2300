c60b9c83e1c5c3c59d912ac44b5f1b097ee24b36
Kurtis Bridgeman <kubridgem@gmail.com>
2014-09-08 10:46:30 -0600

    Lab1 - Base Lab Complete
    - Enhancement section and commenting required

--- Labs/CMPE2300KurtisBridgemanLab1/CMPE2300KurtisBridgemanLab1/Form1.Designer.cs
+++ Labs/CMPE2300KurtisBridgemanLab1/CMPE2300KurtisBridgemanLab1/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.labelResults = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -50,7 +51,7 @@
             this.toolStripComboBox1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(485, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(523, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -67,6 +68,7 @@
             // toolStripButtonDecode
             // 
             this.toolStripButtonDecode.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonDecode.Enabled = false;
             this.toolStripButtonDecode.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDecode.Image")));
             this.toolStripButtonDecode.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButtonDecode.Name = "toolStripButtonDecode";
@@ -77,6 +79,7 @@
             // toolStripButtonDecodeImage
             // 
             this.toolStripButtonDecodeImage.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonDecodeImage.Enabled = false;
             this.toolStripButtonDecodeImage.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDecodeImage.Image")));
             this.toolStripButtonDecodeImage.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButtonDecodeImage.Name = "toolStripButtonDecodeImage";
@@ -93,6 +96,7 @@
             this.toolStripComboBox1.Name = "toolStripComboBox1";
             this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
             this.toolStripComboBox1.Text = "Color";
+            this.toolStripComboBox1.TextChanged += new System.EventHandler(this.toolStripComboBox1_TextChanged);
             // 
             // pictureBox1
             // 
@@ -101,7 +105,8 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(12, 28);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(461, 214);
+            this.pictureBox1.Size = new System.Drawing.Size(499, 288);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -114,17 +119,28 @@
             this.labelResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelResults.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.labelResults.Location = new System.Drawing.Point(13, 249);
+            this.labelResults.Location = new System.Drawing.Point(13, 335);
             this.labelResults.Name = "labelResults";
-            this.labelResults.Size = new System.Drawing.Size(460, 54);
+            this.labelResults.Size = new System.Drawing.Size(498, 54);
             this.labelResults.TabIndex = 2;
             this.labelResults.Text = "Decoded Text";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 322);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(498, 10);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
             // 
             // FormImageSecrets
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 312);
+            this.ClientSize = new System.Drawing.Size(523, 398);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.labelResults);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.toolStrip1);
@@ -148,6 +164,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label labelResults;
+        private System.Windows.Forms.ProgressBar progressBar1;
 
     }
 }
--- Labs/CMPE2300KurtisBridgemanLab1/CMPE2300KurtisBridgemanLab1/Form1.cs
+++ Labs/CMPE2300KurtisBridgemanLab1/CMPE2300KurtisBridgemanLab1/Form1.cs
@@ -33,14 +33,11 @@ namespace CMPE2300KurtisBridgemanLab1
                 bMapOrig = new Bitmap(openFileDialog1.FileName);
                 pictureBox1.Image = bMapOrig;
 
-                
-
-              //  bMap.SetResolution(pictureBox1.Height, pictureBox1.Width); 
-
-                this.Width = bMapOrig.Width;
-                this.Height = bMapOrig.Height;
-
-
+                if (toolStripComboBox1.Text != "Color")
+                {
+                    toolStripButtonDecode.Enabled = true;
+                    toolStripButtonDecodeImage.Enabled = true;
+                }
             }
 
             toolStrip1.Enabled = true;
@@ -51,9 +48,68 @@ namespace CMPE2300KurtisBridgemanLab1
         {
             if (bMapOrig != null)
             {
-                
 
+                Color tmpColor;
+                bMapDecode = new Bitmap(bMapOrig.Width, bMapOrig.Height);
 
+                for (int iirow = 0; iirow < bMapDecode.Width; iirow++)
+                    for (int iicol = 0; iicol < bMapDecode.Height; iicol++)
+                        bMapDecode.SetPixel(iirow, iicol, Color.Black);
+
+                for ( int irow =0; irow<bMapOrig.Width;irow++)
+                    for (int icol = 0; icol < bMapOrig.Height; icol++)
+                    {
+                        tmpColor = bMapOrig.GetPixel(irow, icol);
+
+                        switch (toolStripComboBox1.Text)
+                        {
+                            case "Red":
+                                if (((byte)tmpColor.R & (0x01 << 0)) != 0)
+                                    bMapDecode.SetPixel(irow, icol, Color.Red);
+                                break;
+
+                            case"Green":
+                                if (((byte)tmpColor.G & (0x01 << 0)) != 0)
+                                    bMapDecode.SetPixel(irow, icol, Color.Green);
+                                break;
+
+                            case "Blue":
+                                if (((byte)tmpColor.B & (0x01 << 0)) != 0)
+                                    bMapDecode.SetPixel(irow, icol, Color.Blue);
+                                break;
+
+                            case "All":
+                                if (((byte)tmpColor.R & (0x01 << 0)) != 0)
+                                    bMapDecode.SetPixel(irow, icol, Color.Red);
+                                if (((byte)tmpColor.G & (0x01 << 0)) != 0)
+                                    bMapDecode.SetPixel(irow, icol, Color.Green);
+                                if (((byte)tmpColor.B & (0x01 << 0)) != 0)
+                                    bMapDecode.SetPixel(irow, icol, Color.Blue);
+                                break;
+
+                        }
+                        progressBar1.Maximum = bMapDecode.Width;
+                        progressBar1.Value = irow;
+                    }
+
+                pictureBox1.Image = bMapDecode;
+                progressBar1.Value = 0;
+
+            }
+        }
+
+        private void toolStripComboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (toolStripComboBox1.Text == "Color")
+            {
+                toolStripButtonDecode.Enabled = false;
+                toolStripButtonDecodeImage.Enabled = false;
+            }
+
+            else
+            {
+                toolStripButtonDecode.Enabled = true;
+                toolStripButtonDecodeImage.Enabled = true;
             }
         }
 
