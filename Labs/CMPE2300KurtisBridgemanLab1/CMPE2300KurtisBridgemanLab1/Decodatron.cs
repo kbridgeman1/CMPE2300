0f75b89e4dc4e4122da7b89b06c7d04d8c0a25fa
Kurtis Bridgeman <kubridgem@gmail.com>
2014-09-11 11:45:25 -0600

    Lab1 - Complete
    - commenting started
    - find out how to put your methods into another class

--- Labs/CMPE2300KurtisBridgemanLab1/CMPE2300KurtisBridgemanLab1/Decodatron.cs
+++ Labs/CMPE2300KurtisBridgemanLab1/CMPE2300KurtisBridgemanLab1/Decodatron.cs
@@ -1,7 +1,7 @@
 ﻿//*****************************************************************************************************
-//Program:  Lab 01 - Decodatron.cs
-//Author:   Kurtis Bridgeman
-//Class:    CMPE2300
+//Program:    Lab 01 - Decodatron.cs
+//Author:     Kurtis Bridgeman
+//Class:      CMPE2300
 //*****************************************************************************************************
 
 using System;
@@ -19,114 +19,174 @@ namespace CMPE2300KurtisBridgemanLab1
 {
     public partial class FormImageSecrets : Form
     {
-        private Bitmap bMapOrig;
-        private Bitmap bMapDecode;
+        private Bitmap bMapOrig;            //BitMap used to store the user selected image
+        private Bitmap bMapDecode;          //BitMap used to store the decoded image
 
         public FormImageSecrets()
         {
             InitializeComponent();
         }
 
-        //Occurs when the user presses the "Load Image" tooltip button. Provides the user with an
+        //Occurs when the user presses the "Load Image" toolstrip button. Provides the user with an
         //open file dialog to select a image. The user selected image is assiged to bMapOrig.
         private void toolStripButtonLoadImage_Click(object sender, EventArgs e)
         {
+            //creates a new OpenFileDialog and returns a BitMap
             bMapOrig = OpenFileDialogToBitMap();
 
+            //checks if bMapOrig has been initialized
             if (bMapOrig != null)
             {
+                //checks if the combobox has been changed from its initial value
                 if (toolStripComboBox1.Text != "Color")
+                    //enables the "Decode Image" button
                     toolStripButtonDecode.Enabled = true;
 
+                //displays BMapOrig in the forms picturebox
                 pictureBox1.Image = bMapOrig;
+                
+                //sets the max progressbar value to with width of bMapOrig
                 progressBar1.Maximum = bMapOrig.Width;
+                
+                //enables the "Decode Text" button
                 toolStripButtonDecodeImage.Enabled = true;
+                
+                //clears the text in labelResults
                 labelResults.Text = "";
             }
 
         }
 
+        //Occurs when the user presses the "Decode Image" toolstrip button. Iterates through each
+        //pixel in bMapOrig inspecting the color specified by the comboBox. Sets each pixel in bMapDecode
+        //based on the LSB of the specified color.
         private void toolStripButtonDecodeImage_Click(object sender, EventArgs e)
         {
+            //checks if bMapOrig has been initialized
             if (bMapOrig != null)
             {
+                //creates a new Bitmap with the same dimensions as bMapOrig
                 bMapDecode = new Bitmap(bMapOrig.Width, bMapOrig.Height);
 
+                //iterates through each pixel in bMapOrig
                 for (int irow = 0; irow < bMapOrig.Height; irow++)
                 {
                     for (int icol = 0; icol < bMapOrig.Width; icol++)
                     {
-                        Color tmpColor = bMapOrig.GetPixel(icol, irow);
-                        byte drawColorR = 0;
-                        byte drawColorG = 0;
-                        byte drawColorB = 0;
+                        Color tmpColor = bMapOrig.GetPixel(icol, irow);     //Color for storing the coloe of the current pixel
 
+                        byte drawColorR = 0;                                //byte for storing the Red value of a pixel
+                        byte drawColorG = 0;                                //byte for storing the Green value of a pixel
+                        byte drawColorB = 0;                                //byte for storing the Blue value of a pixel
+
+                        //checks the comboBox for the users color choice
                         switch (toolStripComboBox1.Text)
                         {
                             case "Red":
-                                if (((byte)tmpColor.R & 1) != 0)
+                                //checks if the LSB of the red byte is equal to 1 
+                                if (((byte)tmpColor.R & 1) == 1)
+                                    //sets the current pixel in bMapDecode to red 
                                     bMapDecode.SetPixel(icol, irow, Color.Red);
+                                else
+                                    //sets the current pixel in bMapDecode to black
+                                    bMapDecode.SetPixel(icol, irow, Color.Black);
                                 break;
 
                             case "Green":
-                                if (((byte)tmpColor.G & 1) != 0)
+                                //checks if the LSB of the green byte is equal to 1
+                                if (((byte)tmpColor.G & 1) == 1)
+                                    //sets the current pixel in bMapDecode to green
                                     bMapDecode.SetPixel(icol, irow, Color.Green);
+                                else
+                                    //sets the current pixel in bMapDecode to black
+                                    bMapDecode.SetPixel(icol, irow, Color.Black);
                                 break;
 
                             case "Blue":
-                                if (((byte)tmpColor.B & (1 << 0)) != 0)
+                                //checks if the LSB of the blue byte is equal to 1
+                                if (((byte)tmpColor.B & 1) == 1)
+                                    //sets the current pixel in bMapDecode to blue
                                     bMapDecode.SetPixel(icol, irow, Color.Blue);
+                                else
+                                    //sets the current pixel in bMapDecode to black
+                                    bMapDecode.SetPixel(icol, irow, Color.Black);
                                 break;
 
                             case "All":
-                                if (((byte)tmpColor.R & 1) != 0)
+                                //checks if the LSB of the red byte is equal to 1
+                                if (((byte)tmpColor.R & 1) == 1)
+                                    //sets the red byte value to 255
                                     drawColorR = 255;
-                                if (((byte)tmpColor.G & 1) != 0)
+
+                                //checks if the LSB of the red byte is equal to 1
+                                if (((byte)tmpColor.G & 1) == 1)
+                                    //sets the green byte value to 255
                                     drawColorG = 255;
-                                if (((byte)tmpColor.B & 1) != 0)
+
+                                //checks if the LSB of the red byte is equal to 1
+                                if (((byte)tmpColor.B & 1) == 1)
+                                    //sets the blue byte value to 255
                                     drawColorB = 255;
 
+                                //sets the pixel in bMapDecode to a color using the previous color bytes
                                 bMapDecode.SetPixel(icol, irow, Color.FromArgb(drawColorR, drawColorG, drawColorB));
                                 break;
 
                         }
+                        //updates the forms progress bar after iterating through each row
                         progressBar1.Value = irow;
                     }
                 }
 
+                //displays bMapDecode in the forms picturebox
                 pictureBox1.Image = bMapDecode;
+
+                //resets the progress bar
                 progressBar1.Value = progressBar1.Minimum;
 
             }
         }
 
+        //Occurs when the usesr presses the "Decode Text" toolstrip button. Iterates through each
+        //pixel in bMapOrig inspecting the blue color. Stores the LSB of each byte in a boolean array.
+        //Casts each byte from a byte array as a char and adds it the forms label.
         private void toolStripButtonDecodeText_Click(object sender, EventArgs e)
         {
+            //checks if bMapOrig has been initialized
             if (bMapOrig != null)
             {
-                bool[] bits = new bool[bMapOrig.Width * bMapOrig.Height];
-                int bitCounter = 0;
+                bool[] bits = new bool[bMapOrig.Width * bMapOrig.Height];   //bool array for storing bits
+                int bitIndex = 0;                                           //int used to index the bits array
 
+                //iterates though each pixel in bMapOrig
                 for (int irow = 0; irow < bMapOrig.Height; irow++)
                 {
                     for (int icol = 0; icol < bMapOrig.Width; icol++)
                     {
-                        Color tmpColor = bMapOrig.GetPixel(icol, irow);
+                        Color tmpColor = bMapOrig.GetPixel(icol, irow);     //Color for storing the coloe of the current pixel
 
-                        if (((byte)tmpColor.B & 1) != 0)
-                            bits[bitCounter] = true;
+                        //checks if the LSB of the blue byte is equal to 1
+                        if (((byte)tmpColor.B & 1) == 1)
+                            //assigns true to the bool indexed at bitIndex
+                            bits[bitIndex] = true;
 
-                        bitCounter++;
+                        //add one to the number of bits in the array
+                        bitIndex++;
                     }
 
+                    //updates the forms progress bar after iterating through each row
                     progressBar1.Value = irow;
                 }
 
-                progressBar1.Value = 0;
+                //resets the progress bar
+                progressBar1.Value = progressBar1.Minimum;
                 
+                //iterates through each byte in a byte array returned by BoolArrayToByteArray()
                 foreach (byte b in BoolArrayToByteArray(bits))
                 {
+                    //checks if the value of the byte is 0xFF
                     if (b != byte.MaxValue)
+                        //cast the byte as char and add it to the forms label
                         labelResults.Text += (char)b;
                 }
 
@@ -134,12 +194,17 @@ namespace CMPE2300KurtisBridgemanLab1
 
         }
 
+        //Occurs when the forms comboBox has it's text changed. Enables/disables form controls based
+        //on whether a color or image have been selected.
         private void toolStripComboBox1_TextChanged(object sender, EventArgs e)
         {
+            //checks if toolStripComboBox1 has it's defualt text or bMapOrig is uninitialized
             if (toolStripComboBox1.Text == "Color" || bMapOrig == null)
+                //sets the "Decode Image" button to be disabled
                 toolStripButtonDecode.Enabled = false;
 
             else
+                //sets the "Decode Image" button to be enabled
                 toolStripButtonDecode.Enabled = true;
         }
 
