9c5371823cf8123ee15ce167a29b229d34c99218
Kurtis Bridgeman <kubridgem@gmail.com>
2014-10-24 10:44:58 -0600

    ICA10 - all done

---
+++ Assignments/CMPE2300KurtisBridgemanICA10/CMPE2300KurtisBridgemanICA10/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace CMPE2300KurtisBridgemanICA10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadFile = new System.Windows.Forms.Button();
+            this.trackBarCutoffFreq = new System.Windows.Forms.TrackBar();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderKeyWord = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderFreq = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarCutoffFreq)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoadFile
+            // 
+            this.buttonLoadFile.Location = new System.Drawing.Point(1, 0);
+            this.buttonLoadFile.Name = "buttonLoadFile";
+            this.buttonLoadFile.Size = new System.Drawing.Size(477, 48);
+            this.buttonLoadFile.TabIndex = 0;
+            this.buttonLoadFile.Text = "Load File";
+            this.buttonLoadFile.UseVisualStyleBackColor = true;
+            this.buttonLoadFile.Click += new System.EventHandler(this.buttonLoadFile_Click);
+            // 
+            // trackBarCutoffFreq
+            // 
+            this.trackBarCutoffFreq.Location = new System.Drawing.Point(1, 54);
+            this.trackBarCutoffFreq.Maximum = 100;
+            this.trackBarCutoffFreq.Name = "trackBarCutoffFreq";
+            this.trackBarCutoffFreq.Size = new System.Drawing.Size(477, 45);
+            this.trackBarCutoffFreq.TabIndex = 1;
+            this.trackBarCutoffFreq.Scroll += new System.EventHandler(this.trackBarCutoffFreq_Scroll);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderKeyWord,
+            this.columnHeaderFreq});
+            this.listView1.Location = new System.Drawing.Point(1, 122);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(477, 404);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderKeyWord
+            // 
+            this.columnHeaderKeyWord.Text = "KeyWord";
+            this.columnHeaderKeyWord.Width = 180;
+            // 
+            // columnHeaderFreq
+            // 
+            this.columnHeaderFreq.Text = "Frequency";
+            this.columnHeaderFreq.Width = 180;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Remove less than:  0 occurences";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 526);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.trackBarCutoffFreq);
+            this.Controls.Add(this.buttonLoadFile);
+            this.Name = "Form1";
+            this.Text = "ICA10";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarCutoffFreq)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadFile;
+        private System.Windows.Forms.TrackBar trackBarCutoffFreq;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderKeyWord;
+        private System.Windows.Forms.ColumnHeader columnHeaderFreq;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Assignments/CMPE2300KurtisBridgemanICA10/CMPE2300KurtisBridgemanICA10/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPE2300KurtisBridgemanICA10
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog opnFileDlg;
+
+        SortedDictionary<string, int> sDicKeyWords = new SortedDictionary<string, int>();
+        List<string> lString = new List<string>(System.IO.File.ReadLines(@"..\..\..\KeyWords.txt"));
+        List<string> csString = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            csString.Clear();
+
+            //new open file dialog to recieve a .cs file 
+            if (opnFileDlg != null)
+                opnFileDlg = null;
+
+            opnFileDlg = new OpenFileDialog();
+
+            opnFileDlg.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            opnFileDlg.Filter = "Class Files|*.cs";
+
+            if (opnFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                List<string> tcsLString = new List<string>(System.IO.File.ReadLines(opnFileDlg.FileName));
+
+                //splits the text contents of selected .cs file into a list of strings, representing each word (csString)
+                foreach (string s in tcsLString)
+                    foreach (string ss in s.Split(' ', '(', ')', ';', '=', ',', '.', '{', '}'))
+                    {
+                        if(!String.IsNullOrWhiteSpace(ss))
+                        csString.Add(ss);
+                    }
+            }
+
+          
+            List<string> tLString = new List<string>();
+
+            //splits the contents of the keywords.txt file into a list of strings, representing each work (tLString)
+            foreach (string s in lString)
+                foreach (string ss in s.Split(' '))
+                    tLString.Add(ss);
+
+            foreach ( string s in csString)
+            {
+                if(tLString.Contains(s))
+                {
+                    if (sDicKeyWords.ContainsKey(s))
+                        sDicKeyWords[s] += 1;
+                    else
+                        sDicKeyWords[s] = 1;
+                }
+
+            }
+
+            SortedDictionary<string, int> tDic = new SortedDictionary<string, int>(sDicKeyWords);
+            foreach (KeyValuePair<string, int> kvp in tDic.Where(entry => entry.Value < trackBarCutoffFreq.Value))            
+                sDicKeyWords.Remove(kvp.Key);
+            
+
+
+
+            ShowDictionary();
+        }
+
+        private void ShowDictionary() 
+        {
+            //clears the items from the ListView, then adds then contents of the sorted dictionary ( sDicKeyWords)
+            listView1.Items.Clear();
+
+            foreach (KeyValuePair<string, int> kvp in sDicKeyWords)
+            {
+                ListViewItem lvi = listView1.Items.Add(kvp.Key);
+                lvi.SubItems.Add(kvp.Value.ToString());
+            }
+        }
+
+        private void trackBarCutoffFreq_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = String.Format("Remove less than: {0} occurences", trackBarCutoffFreq.Value);
+        }
+
+
+
+    }
+}
