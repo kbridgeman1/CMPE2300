6876f52e624423a4f1b8dd780ceae6dfda226aee
Kurtis Bridgeman <kubridgem@gmail.com>
2014-10-31 12:04:02 -0600

    ICA11 - Started
    started ICA11, working on create a new drawer window in myRandom default
    constructor

---
+++ Assignments/CMPE2300KurtisBridgemanICA11/CMPE2300KurtisBridgemanICA11/MyRandom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace CMPE2300KurtisBridgemanICA11
+{
+    class MyRandom : Random
+    {
+        protected int MaxSize;
+
+        public MyRandom(int maxsize) : base(maxsize) { }
+
+        public Rectangle NextDrawerRect(CDrawer canv)
+        {
+            int rWidth = Next(10, canv.ScaledWidth);
+            int rHeight = Next(10, canv.ScaledHeight);
+            return new Rectangle(Next(0, canv.ScaledWidth - rWidth), Next(0, canv.ScaledHeight - rHeight), rWidth, rHeight);
+        }
+
+    }
+
+
+    class RectDrawer : GDIDrawer.CDrawer
+    {
+        MyRandom myRND;
+
+        public RectDrawer()
+        {
+            
+        }
+
+    }
+
+
+
+}
---
+++ Demos/InheritDemo/InheritDemo/InheritDemo/Derived.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritDemo
+{
+	class myRandom : Random
+	{
+		public myRandom(int iSeed) : base(iSeed) { }
+		public double getDouble( double dMax )
+		{
+			//Random r = Random(); IS A !!!!!
+			return NextDouble() * dMax;
+		}
+	}
+	class myDrawer : GDIDrawer.CDrawer
+	{
+
+	}
+}
