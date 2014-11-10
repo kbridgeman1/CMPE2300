98849c010ede9f6f02cdb41bfad77228923d0013
Kurtis Bridgeman <kubridgem@gmail.com>
2014-11-10 11:09:34 -0700

    ICA12 - done
    read the notes

---
+++ Assignments/CMPE2300KurtisBridgemanICA12/CMPE2300KurtisBridgemanICA12/Ball.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace CMPE2300KurtisBridgemanICA12
+{
+    class Ball
+    {
+        //static variables
+        static private Random rnd = new Random();
+
+        //instance variables
+        private Point _ballLocation;
+
+        private int _ballRadius;
+        private int _ballXVelocity;
+        private int _ballYVelocity;   
+
+        //instance properties
+        public Color BallColor { get; set; }
+
+        //instance constructor
+        public Ball(Point location, Color color)
+        {
+            _ballLocation.X = location.X;
+            _ballLocation.Y = location.Y;
+            _ballXVelocity = rnd.Next(-5, 6);
+            _ballYVelocity = rnd.Next(-5, 6);
+            _ballRadius = rnd.Next(20, 51);
+            BallColor = color;
+        }
+
+
+        //instance methods
+        public void Move(CDrawer canvas)
+        {
+            if (_ballLocation.X + _ballXVelocity >= canvas.ScaledWidth - _ballRadius)
+            {
+                _ballLocation.X = canvas.ScaledWidth - _ballRadius;
+                _ballXVelocity *= -1;
+            }
+
+            if (_ballLocation.X + _ballXVelocity <= _ballRadius)
+            {
+                _ballLocation.X = _ballRadius;
+                _ballXVelocity *= -1;
+            }
+
+            if (_ballLocation.Y + _ballYVelocity >= canvas.ScaledHeight - _ballRadius)
+            {
+                _ballLocation.Y = canvas.ScaledHeight - _ballRadius;
+                _ballYVelocity *= -1;
+            }
+
+            if (_ballLocation.Y + _ballYVelocity <= _ballRadius)
+            {
+                _ballLocation.Y = _ballRadius;
+                _ballYVelocity *= -1;
+            }
+
+            _ballLocation.X += _ballXVelocity;
+            _ballLocation.Y += _ballYVelocity;
+        }
+
+        public void Show(CDrawer canvas, int ballCount)
+        {
+            Color complimentColor = Color.FromArgb(BallColor.ToArgb() ^ 0x00FFFFFF);
+            
+            canvas.AddCenteredEllipse(_ballLocation.X, _ballLocation.Y, _ballRadius * 2, _ballRadius * 2, BallColor);
+            canvas.AddText(ballCount.ToString(), 14, _ballLocation.X - _ballRadius, _ballLocation.Y - _ballRadius, _ballRadius*2, _ballRadius*2, complimentColor);
+        }
+
+        //override functions
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Ball))
+                return false;
+
+            Ball testBall = (Ball)obj;
+
+            if (Math.Sqrt((Math.Pow((_ballLocation.X - testBall._ballLocation.X), 2)) + (Math.Pow((_ballLocation.Y - testBall._ballLocation.Y), 2))) <= _ballRadius + testBall._ballRadius)
+                return true;
+
+            else
+                return false;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+    }
+}
---
+++ Assignments/CMPE2300KurtisBridgemanICA12/CMPE2300KurtisBridgemanICA12/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPE2300KurtisBridgemanICA12
+{
+    public partial class Form1 : Form
+    {
+        PicDrawer canvasA;
+        RectDrawer canvasB;
+
+        //CDrawer canvasA = new CDrawer(800, 500, false, false);
+        //CDrawer canvasB = new CDrawer(800, 500, false, false);
+
+        List<Ball> bBalls = new List<Ball>();
+        List<Ball> gBalls = new List<Ball>();
+        List<Ball> rBalls = new List<Ball>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            canvasA = new PicDrawer(Properties.Resources.ImageInImageMedium);
+            canvasB = new RectDrawer(canvasA.ScaledWidth, canvasA.ScaledHeight);
+
+
+
+            timer1.Enabled = true;
+            timer1.Interval = 30;
+
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(0, canvasA.ScaledHeight + 50);
+
+            Point p = new Point(0, 0);
+            canvasA.Position = p;
+            p.X = canvasA.ScaledWidth + 20;
+            canvasB.Position = p;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            Point mouseLocation;
+
+            bool bLMouseClick = canvasA.GetLastMouseLeftClickScaled(out mouseLocation);
+
+            if (bLMouseClick)
+            {
+                Ball tBall = new Ball(mouseLocation, Color.Green);
+
+                if (!gBalls.Contains(tBall))
+                    gBalls.Add(tBall);
+            }
+
+            bool bRMouseClick = canvasA.GetLastMouseRightClickScaled(out mouseLocation);
+
+            if (bRMouseClick)
+            {
+                Ball tBall = new Ball(mouseLocation, Color.Red);
+
+                if (-1 == bBalls.IndexOf(tBall))
+                    bBalls.Insert(0, tBall);
+            }
+
+            foreach (Ball b in bBalls)
+                b.Move(canvasA);
+
+            foreach (Ball b in gBalls)
+                b.Move(canvasA);
+
+            foreach (Ball b in rBalls)
+                b.Move(canvasB);
+
+            List<Ball> tBalls = bBalls.Intersect(gBalls).ToList();
+
+            foreach (Ball b in tBalls)
+            {
+                while (bBalls.Remove(b)) ;
+                while (gBalls.Remove(b)) ;
+
+                b.BallColor = Color.Yellow;
+            }
+
+            rBalls = new List<Ball>(rBalls.Union(tBalls));
+
+            canvasA.Clear();
+            canvasA.AddText(String.Format("Blue: {0} {1} Green: {2}", bBalls.Count, Environment.NewLine, gBalls.Count), 50, Color.FromArgb(100, Color.Cyan));
+
+            for (int i = 0; i < bBalls.Count; i++)
+                bBalls[i].Show(canvasA, i);
+
+            for (int ii = 0; ii < gBalls.Count; ii++)
+                gBalls[ii].Show(canvasA, ii);
+
+            canvasA.Render();
+
+            canvasB.Clear();
+            canvasB.AddText(String.Format("Red: {0}", rBalls.Count), 50, Color.FromArgb(100, Color.Cyan));
+
+            for (int iii = 0; iii < rBalls.Count; iii++)
+                rBalls[iii].Show(canvasB, iii);
+
+            canvasB.Render();
+        }
+
+
+    }
+}
