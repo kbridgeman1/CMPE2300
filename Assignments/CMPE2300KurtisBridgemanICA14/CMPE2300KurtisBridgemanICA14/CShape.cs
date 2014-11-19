a7431d78eb95cb5d9de5df6df6185060a90c5af2
Kurtis Bridgeman <kubridgem@gmail.com>
2014-11-19 15:02:58 -0700

    ICA14 & ICA15 Started

---
+++ Assignments/CMPE2300KurtisBridgemanICA14/CMPE2300KurtisBridgemanICA14/CShape.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+using KurtisBridgeman_Drawers;
+
+
+namespace CMPE2300KurtisBridgemanICA14
+{
+    abstract class CShape
+    {
+        public static PicDrawer Canvas { get; set; }
+        protected static Random _rnd;
+        protected Point _pLocation;
+        protected Color _color = RandColor.GetColor();
+        protected int _iRadius;
+
+        static CShape()
+        {
+            _rnd = new Random(0);
+        }
+
+        public CShape(Point pt)
+        {
+            _pLocation = pt;
+            _iRadius = _rnd.Next(25, 51);
+        }
+
+        //public interfaces   these are not actually interfaces though
+        public void Render()
+        {
+            VirtualRender();
+        }
+        public void iMoveBall()
+        {
+            Move();
+        }
+        public void iAnimate()
+        {
+            Animate();
+        }
+
+        //Non-virtual interfaces
+        protected abstract void VirtualRender();
+        protected abstract void Move();
+        protected abstract void Animate();
+    }
+
+
+
+    class MovingBall : CShape
+    {
+
+        protected override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Assignments/CMPE2300KurtisBridgemanICA15/CMPE2300KurtisBridgemanICA15/Gate.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPE2300KurtisBridgemanICA15
+{
+    public abstract class Gate
+    {
+        //protected properties for gate ins/outs
+        protected bool inA
+        {
+            get { return inA; }
+            set { inA = value; }
+        }
+
+        protected bool inB
+        {
+            get { return inB; }
+            set { inB = value; }
+        }
+
+        protected bool outAB
+        {
+            get { return outAB; }
+            set { outAB = value; }
+        }
+
+        //public methods
+        public void Set(bool A, bool B)
+        {
+            inA = A;
+            inB = B;
+        }
+
+        //public user interface
+        public void Latch()
+        {
+            vLatch();
+        }
+        public string Name()
+        {
+            return vName();
+        }
+
+        //non-virtual interface
+        protected abstract void vLatch();
+        protected abstract string vName();
+    }
+
+
+    public class NandGate : Gate
+    {
+        protected override void vLatch()
+        {
+            outAB = !(inA && inB);
+        }
+
+        protected override string vName()
+        {
+            return "NAND";
+        }
+    }
+
+    public class OrGate : Gate
+    {
+        protected override void vLatch()
+        {
+            outAB = (inA || inB);
+        }
+
+        protected override string vName()
+        {
+            return "OR";
+        }
+    }
+
+    public class XorGate : Gate
+    {
+        protected override void vLatch()
+        {
+            outAB = ((inA || inB) && !(inA && inB));
+        }
+
+        protected override string vName()
+        {
+            return "XOR";
+        }
+    }
+
+    public class AndGate : NandGate
+    {
+        protected override void vLatch()
+        {
+            base.Latch();
+            outAB = !outAB;
+        }
+
+        protected override string vName()
+        {
+            return "AND";
+        }
+    }
+
+}
