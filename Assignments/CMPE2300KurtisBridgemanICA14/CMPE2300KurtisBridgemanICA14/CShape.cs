c9ed2609834d023a32d608bb56c0e05bf77e0243
Kurtis Bridgeman <kubridgem@gmail.com>
2014-11-26 15:04:25 -0700

    ICA14 - almost complete
    comment your lab

--- Assignments/CMPE2300KurtisBridgemanICA14/CMPE2300KurtisBridgemanICA14/CShape.cs
+++ Assignments/CMPE2300KurtisBridgemanICA14/CMPE2300KurtisBridgemanICA14/CShape.cs
@@ -10,6 +10,18 @@ using KurtisBridgeman_Drawers;
 
 namespace CMPE2300KurtisBridgemanICA14
 {
+
+    internal interface IMoveable
+    {
+        void Move();
+    }
+
+    internal interface IAnimate
+    {
+        void Animate();
+    }
+
+
     abstract class CShape
     {
         public static PicDrawer Canvas { get; set; }
@@ -34,30 +46,78 @@ namespace CMPE2300KurtisBridgemanICA14
         {
             VirtualRender();
         }
-        public void iMoveBall()
-        {
-            Move();
-        }
-        public void iAnimate()
-        {
-            Animate();
-        }
 
         //Non-virtual interfaces
         protected abstract void VirtualRender();
-        protected abstract void Move();
-        protected abstract void Animate();
+
     }
 
-
-
-    class MovingBall : CShape
+    //derived from CShape, supports IMovable interface
+    class MovingBall : CShape, IMoveable
     {
-
-        protected override void Move()
+        int xVel;
+        int yVel;
+        
+        //custom constructor leveraging base constructor, to initialize radius and location
+        public MovingBall(Point p)
+            : base(p)
         {
-            throw new NotImplementedException();
+            xVel = _rnd.Next(-8, 9); //random x velocity
+            yVel = _rnd.Next(-8, 9); //random y velocity
         }
+
+        //supports IMoveable, so we ,must provide a Move() function
+        public void Move()
+        {
+            if (_pLocation.X + xVel >= Canvas.ScaledWidth - _iRadius || _pLocation.X + xVel <= _iRadius)
+                xVel *= -1;
+
+            if (_pLocation.Y + yVel >= Canvas.ScaledHeight - _iRadius || _pLocation.Y + yVel < _iRadius)
+                yVel *= -1;
+
+            _pLocation.X += xVel;
+            _pLocation.Y += yVel;
+
+        }
+
+        
+        protected override void VirtualRender()
+        {
+            Canvas.AddCenteredEllipse(_pLocation.X, _pLocation.Y, _iRadius / 2, _iRadius / 2, _color);
+        }
+
+
+
+
     }
+
+    //derived from CShape, supports IAnimate interface
+    class SpinnerBall: CShape, IAnimate
+    {
+        double drawAngle;
+        double incAngle;
+
+        //custom constructor leveraging base constructor, to initialize radius and location
+        public SpinnerBall(Point p)
+            : base(p)
+        {
+            incAngle = _rnd.NextDouble() * 0.2;
+        }
+
+        //supports IAnimate , so we must provide a Animate() function
+        public void Animate()
+        {
+            drawAngle += incAngle;
+        }
+
+        protected override void VirtualRender()
+        {
+            Canvas.AddCenteredEllipse(_pLocation.X, _pLocation.Y, _iRadius / 2, _iRadius / 2, _color);
+            Canvas.AddLine(_pLocation, _iRadius, drawAngle, _color, 5);
+        }
+
+    }
+
+
 
 }
--- Assignments/CMPE2300KurtisBridgemanICA14/CMPE2300KurtisBridgemanICA14/Form1.cs
+++ Assignments/CMPE2300KurtisBridgemanICA14/CMPE2300KurtisBridgemanICA14/Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using KurtisBridgeman_Drawers;
 
 namespace CMPE2300KurtisBridgemanICA14
 {
     public partial class Form1 : Form
     {
+
+        List<CShape> shapeList;
+
         public Form1()
         {
             InitializeComponent();
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+            shapeList = new List<CShape>();
+
+            CShape.Canvas = new PicDrawer(Properties.Resources.ImageInImageMedium);
+            CShape.Canvas.Render();
+
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Point msLocation;
+
+            CShape.Canvas.GetLastMouseLeftClickScaled(out msLocation);
+          
+
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
