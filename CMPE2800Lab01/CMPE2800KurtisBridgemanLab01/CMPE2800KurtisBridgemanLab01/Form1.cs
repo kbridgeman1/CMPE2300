5939bd4ee6d9bb0f51a4bd12c083c0998a8bf5c5
Kurtis Bridgeman <kubridgem@gmail.com>
2015-01-09 12:11:14 -0700

    2800Lab01 2/3 done

--- CMPE2800Lab01/CMPE2800KurtisBridgemanLab01/CMPE2800KurtisBridgemanLab01/Form1.cs
+++ CMPE2800Lab01/CMPE2800KurtisBridgemanLab01/CMPE2800KurtisBridgemanLab01/Form1.cs
@@ -7,16 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 using GDIDrawer;
+
 
 namespace CMPE2800KurtisBridgemanLab01
 {
     public partial class Form1 : Form
     {
-        CDrawer canvas;
+        static CDrawer canvas;
         Color[,] colArray;
+        Thread m_tThread;
         volatile bool dirtFlg;
-        object lockObj =  new object();
+        object lockObj = new object();
 
         public Form1()
         {
@@ -25,53 +28,114 @@ namespace CMPE2800KurtisBridgemanLab01
             canvas = new CDrawer(bContinuousUpdate: false);
             canvas.Scale = 100;
             canvas.BBColour = Color.Salmon;
-            
 
             colArray = new Color[canvas.ScaledWidth, canvas.ScaledHeight];
             for (int iR = 0; iR < canvas.ScaledHeight; iR++)
                 for (int iC = 0; iC < canvas.ScaledWidth; iC++)
                     colArray[iC, iR] = Color.Salmon;
 
-            for (int i = 0; i < canvas.ScaledWidth; i++)
-                canvas.AddLine(i, 0, i, canvas.ScaledWidth, Color.Black, 1);
+            DrawGridLines();
 
-            for (int i = 0; i < canvas.ScaledHeight; i++)
-                canvas.AddLine(0, i, canvas.ScaledWidth, i, Color.Black, 1);
+            m_tThread = new Thread(Rendering);
+            m_tThread.IsBackground = true;
+            m_tThread.Start();
 
-
-            canvas.Render();
+            m_tThread = new Thread(Clicking);
+            m_tThread.IsBackground = true;
+            m_tThread.Start();
         }
 
         //checks the dirty flag, if true, clears/renders our drawer then sets dirty flag to false
-        private void Rendering(object colorArr)
+        private void Rendering()
         {
+            System.Diagnostics.Trace.WriteLine("Rendering Started");
+
             do
             {
                 if (dirtFlg)
                 {
+
+                    Color[,] tColorArr = colArray as Color[,];
+
                     lock (lockObj)
                     {
-                        Color[,] tColorArr = colorArr as Color[,];
-
                         canvas.Clear();
                         for (int iR = 0; iR < canvas.ScaledHeight; iR++)
                             for (int iC = 0; iC < canvas.ScaledWidth; iC++)
                                 canvas.SetBBScaledPixel(iC, iR, tColorArr[iC, iR]);
 
-                        canvas.Render();
-                        dirtFlg = false;
+                        DrawGridLines();
                     }
+
+                    dirtFlg = false;
                 }
 
-                System.Threading.Thread.Sleep(1);
+                Thread.Sleep(1);
+
+            } while (true);
+
+            System.Diagnostics.Trace.WriteLine("Rendering Ended");
+        }
+
+
+        private void Clicking(object ob)
+        {
+            System.Diagnostics.Trace.WriteLine("Clicking Started");
+            Point msCLocation;
+
+            do
+            {
+                lock (lockObj)
+                    if (canvas.GetLastMouseLeftClickScaled(out msCLocation)
+                     && (colArray[msCLocation.X, msCLocation.Y] == Color.Salmon))
+                    {
+                        colArray[msCLocation.X, msCLocation.Y] = Color.Green;
+                        dirtFlg = true;
+                    }
+
+                lock (lockObj)
+                    if (canvas.GetLastMouseRightClickScaled(out msCLocation)
+                     && (colArray[msCLocation.X, msCLocation.Y] == Color.Green))
+                    {
+                        colArray[msCLocation.X, msCLocation.Y] = Color.Salmon;
+                        dirtFlg = true;
+                    }
+
+                Thread.Sleep(1);
+
+            } while (true);
+
+            System.Diagnostics.Trace.WriteLine("Clicking Ended");
+        }
+
+        private void Moving()
+        {
+            Point msLocation;
+
+            do
+            {
+                canvas.GetLastMousePosition(out msLocation);
+
+
+
+                Thread.Sleep(1);
 
             } while (true);
         }
 
 
 
+        //helper methods
+        private void DrawGridLines()
+        {
+            for (int i = 0; i < canvas.ScaledWidth; i++)
+                canvas.AddLine(i, 0, i, canvas.ScaledWidth, Color.Black, 1);
 
+            for (int i = 0; i < canvas.ScaledHeight; i++)
+                canvas.AddLine(0, i, canvas.ScaledWidth, i, Color.Black, 1);
 
+            canvas.Render();
+        }
 
     }
 }
