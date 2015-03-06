ab41d35fc257ae914ce82810fc2e73ee71a8d12b
Kurtis Bridgeman <kubridgem@gmail.com>
2015-03-06 11:05:29 -0700

    CMPE2500 - ICA09 started

---
+++ CMPE2500/CMPE2500KurtisBridgemanICA09/KurtisBridgemanICA09.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="KurtisBridgemanICA09.aspx.cs" Inherits="KurtisBridgemanICA09" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>ICA09 - AspImgur</h1><hr />
+        <asp:MultiView ID="MultViewMain" runat="server">
+            <asp:View ID="ViewLogin" runat="server">
+                <asp:Label ID="v1lblUsername" runat="server" Text="User Name:"></asp:Label>
+                <asp:TextBox ID="v1tbxUsername" runat="server"></asp:TextBox>
+                <asp:Label ID="v1lblPassword" runat="server" Text="Password:"></asp:Label>
+                <asp:TextBox ID="v1tBxPassword" runat="server" TextMode="Password"></asp:TextBox>
+                <asp:Button ID="v1btnNext" runat="server" Text="Next" OnClick="v1btnNext_Click" />
+            </asp:View>
+            <asp:View ID="ViewAddImages" runat="server">
+                <asp:Label ID="v2lblMessage" runat="server" Text="Label"></asp:Label>
+                <asp:FileUpload ID="v2FileUploadAddImage" runat="server" /><br />
+                <asp:Button ID="v2btnNext" runat="server" Text="Next" OnClick="v2btnNext_Click" />
+            </asp:View>
+
+
+
+        </asp:MultiView>
+        <asp:Label ID="lblStatus" runat="server" Text="Label"></asp:Label>
+        <asp:Button ID="btnToAlbum" runat="server" Text="Go To Album" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ CMPE2500/CMPE2500KurtisBridgemanICA09/KurtisBridgemanICA09.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.IO;
+
+public partial class KurtisBridgemanICA09 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            lblStatus.Text = "Lets Begin";
+            lblStatus.Width = Unit.Percentage(100);
+            lblStatus.BorderStyle = BorderStyle.Inset;
+            lblStatus.BackColor = Color.LightGreen;
+
+            MultViewMain.ActiveViewIndex = 0;
+        }
+
+    }
+    protected void v1btnNext_Click(object sender, EventArgs e)
+    {
+
+        if (v1tbxUsername.Text.Length < 1)
+        {
+            lblStatus.Text = "Please enter a username.";
+            lblStatus.BackColor = Color.Red;
+            SetFocus(v1tbxUsername);
+        }
+
+        else if (v1tBxPassword.Text.Length < 1)
+        {
+            lblStatus.Text = "Please enter a password.";
+            lblStatus.BackColor = Color.Red;
+            SetFocus(v1tBxPassword);
+        }
+
+        else
+        {
+            MultViewMain.ActiveViewIndex = 1;
+            lblStatus.Text = "Add pictures.";
+            lblStatus.BackColor = Color.LightGreen;
+            v2lblMessage.Text = String.Format("Thanks {0}, Now add to your album:", v1tbxUsername.Text);
+        }
+    }
+    protected void v2btnNext_Click(object sender, EventArgs e)
+    {
+
+        string absolutePath;
+        //are these the same?
+        //Server.MapPath(String.Format("~/Uploads/{0}", v1tbxUsername.Text));
+        absolutePath = MapPath(String.Format("~/Uploads/{0}", v1tbxUsername.Text));
+        
+        if(v2FileUploadAddImage.HasFile)
+        {
+            if(!Directory.Exists(absolutePath))
+            {
+                Directory.CreateDirectory(absolutePath);
+            }
+        }
+
+
+    }
+}
